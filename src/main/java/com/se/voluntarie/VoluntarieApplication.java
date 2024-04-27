0efa03c758009ac13e98c1b3e0eea6f70e73b707
luizfilipe29 <luiz_filipe.29@hotmail.com>
2024-04-27 16:01:02 -0300

    [feat] - first commit
    git status

---
+++ src/main/java/com/se/voluntarie/VoluntarieApplication.java
@@ -0,0 +1,13 @@
+package com.se.voluntarie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoluntarieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoluntarieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/se/voluntarie/VoluntarieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.se.voluntarie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoluntarieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
